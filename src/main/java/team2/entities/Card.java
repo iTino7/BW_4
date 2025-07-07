2ff6e1ccbe7baa1d9a9e78386d1c91489a27d5f4
Dan Russo <rdaniele669@gmail.com>
2025-07-07 16:50:46 +0200

    Card and User to Entity

--- src/main/java/team2/entities/Card.java
+++ src/main/java/team2/entities/Card.java
@@ -1,11 +1,24 @@
 package team2.entities;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "cards")
+
 public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name = "activation_dates")
     private LocalDate activationDate;
+    @Column(name = "expiring_dates")
     private LocalDate expiringDate;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User owner;
 
     public Card() {
     }
