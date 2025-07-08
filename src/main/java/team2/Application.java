bbd33af36f47e9dd1b7b5f8ca0f1b040caaf3207
Dan Russo <rdaniele669@gmail.com>
2025-07-08 15:11:52 +0200

    adds CardDAO
    adds CardDAO (save, findByID, delete) and successfully add a newCard to DB

--- src/main/java/team2/Application.java
+++ src/main/java/team2/Application.java
@@ -3,8 +3,12 @@ package team2;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import team2.dao.CardDAO;
 import team2.dao.UserDAO;
+import team2.entities.Card;
 import team2.entities.User;
+
+import java.time.LocalDate;
 
 public class Application {
 
@@ -15,8 +19,13 @@ public class Application {
 
         UserDAO ud = new UserDAO(em);
         User user1 = new User("Mario Rossi");
+        User user1FromDB = ud.findUserByID(1);
+
+        CardDAO cd = new CardDAO(em);
+        Card card1 = new Card(LocalDate.now(), user1FromDB);
 
         //ud.saveUser(user1);
+        cd.saveCard(card1);
 
         System.out.println("Hello World!");
     }
