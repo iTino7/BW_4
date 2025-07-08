8de96466d5111e163a67eaf293db97d5ac68d1d9
iTino7 <Sabatino.b007@outlook.it>
2025-07-08 09:38:13 +0200

    Update class TransportDao

--- src/main/java/team2/dao/TransportDAO.java
+++ src/main/java/team2/dao/TransportDAO.java
@@ -1,4 +1,28 @@
 package team2.dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import team2.entities.Transport;
+
 public class TransportDAO {
+
+    private EntityManager em;
+
+    public TransportDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void save(Transport transport) {
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        em.persist(transport);
+        et.commit();
+    }
+
+    public Transport findById(long id) {
+        return em.find(Transport.class, id);
+    }
+
+
+
 }
