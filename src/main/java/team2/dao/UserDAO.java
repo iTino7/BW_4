84fa9c40c145604a5048d4a55a5f89400e8ef1ca
Dan Russo <rdaniele669@gmail.com>
2025-07-08 12:52:38 +0200

    adds UserDAO
    adds UserDAO (save, findByID, delete) and successfully add a newUser to D

---
+++ src/main/java/team2/dao/UserDAO.java
@@ -0,0 +1,38 @@
+package team2.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import team2.entities.User;
+import team2.exceptions.NotFoundException;
+
+public class UserDAO {
+    private final EntityManager entityManager;
+
+    public UserDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public void saveUser(User newUser) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.persist(newUser);
+        transaction.commit();
+        System.out.println("New user with id " + newUser.getId() + " added successfully!");
+    }
+
+    public User findUserByID(long id) {
+        User found = entityManager.find(User.class, id);
+        if (found == null) throw new NotFoundException(id);
+        return found;
+    }
+
+    public void deleteUserByID(long id) {
+        User found = this.findUserByID(id);
+        if (found == null) throw new NotFoundException(id);
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.remove(found);
+        transaction.commit();
+        System.out.println("User with id " + found.getId() + " successfully removed!");
+    }
+}
