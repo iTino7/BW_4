bffad0e780f820b65213063eeeb5b224117b6700
iTino7 <Sabatino.b007@outlook.it>
2025-07-08 09:41:01 +0200

    Update findById in TransportDAO

--- src/main/java/team2/dao/TransportDAO.java
+++ src/main/java/team2/dao/TransportDAO.java
@@ -3,6 +3,7 @@ package team2.dao;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 import team2.entities.Transport;
+import team2.exceptions.NotFoundException;
 
 public class TransportDAO {
 
@@ -20,9 +21,10 @@ public class TransportDAO {
     }
 
     public Transport findById(long id) {
-        return em.find(Transport.class, id);
+        Transport found = em.find(Transport.class, id);
+        if (found == null) throw new NotFoundException(id);
+        return found;
     }
-
 
 
 }
