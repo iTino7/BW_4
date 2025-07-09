d49721a27301e56ef9a63b4f8925d4ebb266d541
gaetanopyro <gaetano.miscio@gmail.com>
2025-07-09 16:59:51 +0200

    add TrasportRoute
    create query media

--- src/main/java/team2/Application.java
+++ src/main/java/team2/Application.java
@@ -10,6 +10,7 @@ import team2.entities.enums.ResellerStatusType;
 import team2.entities.enums.TransportStatus;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Application {
 
@@ -22,8 +23,8 @@ public class Application {
         User user1 = new User("Mario Rossi");
         User user2 = new User("Giuseppe Verdi");
 
-        //ud.saveUser(user1);
-        //ud.saveUser(user2);
+        ud.saveUser(user1);
+        ud.saveUser(user2);
         User user1FromDB = ud.findUserByID(1);
         User user2FromDB = ud.findUserByID(2);
 
@@ -31,14 +32,14 @@ public class Application {
         Card card1 = new Card(LocalDate.of(2024, 06, 25), user1FromDB);
         Card card2 = new Card(LocalDate.of(2025, 05, 18), user2FromDB);
 
-        //cd.saveCard(card1);
-        //cd.saveCard(card2);
+        cd.saveCard(card1);
+        cd.saveCard(card2);
 
         TransportDAO td = new TransportDAO(em);
         Transport bus = new Bus(40, TransportStatus.IN_SERVICE, LocalDate.of(2024, 12, 10), 100);
         Transport tram = new Tram(60, TransportStatus.UNDER_MAINTENANCE, LocalDate.of(1993, 10, 30), 276);
-        //td.save(bus);
-        //td.save(tram);
+        td.save(bus);
+        td.save(tram);
         Transport busFromDB = td.findById(1);
         Transport tramFromDB = td.findById(2);
 
@@ -48,10 +49,10 @@ public class Application {
         TravelTicket pass1 = new Pass(PassType.MONTHLY, LocalDate.now().plusMonths(1), LocalDate.now());
         TravelTicket pass2 = new Pass(PassType.WEEKLY, LocalDate.now().plusDays(7), LocalDate.now());
 
-        //ttd.save(ticket1);
-        //ttd.save(ticket2);
-        //ttd.save(pass1);
-        //ttd.save(pass2);
+        ttd.save(ticket1);
+        ttd.save(ticket2);
+        ttd.save(pass1);
+        ttd.save(pass2);
 
         RouteDAO rd = new RouteDAO(em);
         Route route1 = new Route("Central Station", "Bridge", 25.10);
@@ -59,10 +60,10 @@ public class Application {
         Route route3 = new Route("Central Park", "Lake", 15.30);
         Route route4 = new Route("Cathedral", "Hospital", 35.50);
 
-        //rd.save(route1);
-        //rd.save(route2);
-        //rd.save(route3);
-        //rd.save(route4);
+        rd.save(route1);
+        rd.save(route2);
+        rd.save(route3);
+        rd.save(route4);
         Route route1FromDB = rd.findById(1);
         Route route2FromDB = rd.findById(2);
 
@@ -72,25 +73,31 @@ public class Application {
         Reseller atm1 = new AutomaticMachine(ResellerStatusType.IN_SERVICE, 25, 30);
         Reseller atm2 = new AutomaticMachine(ResellerStatusType.OUT_OF_ORDER, 35, 40);
 
-        //rld.save(tabacchi1);
-        //rld.save(tabacchi2);
-        //rld.save(atm1);
-        //rld.save(atm2);
+        rld.save(tabacchi1);
+        rld.save(tabacchi2);
+        rld.save(atm1);
+        rld.save(atm2);
 
 
         MaintenanceDAO md = new MaintenanceDAO(em);
         Maintenance maintenance1 = new Maintenance(LocalDate.of(2025, 2, 5), LocalDate.of(2025, 2, 12), busFromDB);
         Maintenance maintenance2 = new Maintenance(LocalDate.of(2025, 3, 8), null, tramFromDB);
 
-        //md.save(maintenance1);
-        //md.save(maintenance2);
+        md.save(maintenance1);
+        md.save(maintenance2);
 
         TransportsRoutesDAO trd = new TransportsRoutesDAO(em);
-        TransportRoute transportRoutes1 = new TransportRoute(route1FromDB, busFromDB, 30.10);
-        TransportRoute transportRoutes2 = new TransportRoute(route2FromDB, tramFromDB, 45.10);
+        TransportRoute transportRoutes1 = new TransportRoute(route1FromDB, busFromDB, 30.10, LocalDateTime.now().minusHours(3));
+        TransportRoute transportRoutes2 = new TransportRoute(route1FromDB, tramFromDB, 45.10, LocalDateTime.now().minusHours(1));
+        TransportRoute transportRoutes3 = new TransportRoute(route1FromDB, busFromDB, 32.10, LocalDateTime.now().minusHours(2));
+        TransportRoute transportRoutes4 = new TransportRoute(route2FromDB, busFromDB, 47.10, LocalDateTime.now().minusMinutes(30));
+        TransportRoute transportRoutes5 = new TransportRoute(route2FromDB, busFromDB, 49.10, LocalDateTime.now().minusMinutes(15));
 
-        //trd.save(transportRoutes1);
-        //trd.save(transportRoutes2);
+        trd.save(transportRoutes1);
+        trd.save(transportRoutes2);
+        trd.save(transportRoutes3);
+        trd.save(transportRoutes4);
+        trd.save(transportRoutes5);
 
 
         System.out.println("**************** METODO CERCA N.OF TICKET *******************");
@@ -116,13 +123,25 @@ public class Application {
 
         System.out.println("Numero di volte che un mezzo (Bus ID 1) ha percorso una tratta (Route ID 1)");
         long bus1Route1Runs = trd.countRunsByTransportAndRoute(busFromDB.getTransport_id(), route1FromDB.getRoute_id());
-        System.out.println("The Bus (ID: " + busFromDB.getTransport_id() + ") has traveled the route '" +
-                route1FromDB.getDeparturePoint() + " - " + route1FromDB.getTerminusRoute() + "' " + bus1Route1Runs + " times.");
+        System.out.println("The Bus (ID: " + busFromDB.getTransport_id() + ") has traveled the route " +
+                route1FromDB.getDeparturePoint() + " - " + route1FromDB.getTerminusRoute() + " " + bus1Route1Runs + " times.");
 
         System.out.println("Numero di volte che un mezzo (Tram ID 2) ha percorso una tratta (Route ID 1)");
         long tram2Route1Runs = trd.countRunsByTransportAndRoute(tramFromDB.getTransport_id(), route1FromDB.getRoute_id());
-        System.out.println("The Tram (ID: " + tramFromDB.getTransport_id() + ") has traveled the route '" +
+        System.out.println("The Tram (ID: " + tramFromDB.getTransport_id() + ") has traveled the route " +
                 route1FromDB.getDeparturePoint() + " - " + route1FromDB.getTerminusRoute() + "' " + tram2Route1Runs + " times.");
+
+        System.out.println("Tempo medio percorrenza rotta");
+        Double avgBus1Route1Time = trd.currentAverageTime(busFromDB.getTransport_id(), route1FromDB.getRoute_id());
+        if (avgBus1Route1Time != null) {
+            double roundedAvgTime = Math.round(avgBus1Route1Time * 100.0) / 100.0;
+            System.out.println("Average travel time for the Bus (ID: " + busFromDB.getTransport_id() +
+                    ") on the route " + route1FromDB.getDeparturePoint() + " - " + route1FromDB.getTerminusRoute() +
+                    "': " + roundedAvgTime + " minutes.");
+        } else {
+            System.out.println("No route found for the Bus (ID: " + busFromDB.getTransport_id() +
+                    ") on the route " + route1FromDB.getDeparturePoint() + " - " + route1FromDB.getTerminusRoute() + " .");
+        }
 
         System.out.println("Hello World!");
 
---
+++ src/main/java/team2/exceptions/NoResultException.java
@@ -0,0 +1,9 @@
+package team2.exceptions;
+
+public class NoResultException extends RuntimeException {
+
+    public NoResultException(String msg) {
+        super(msg);
+    }
+
+}
